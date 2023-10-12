986d10c83a6cbf27c601c80ad6395b64b895daa0
RNGDebeloper <131392755+RNGDebeloper@users.noreply.github.com>
2023-10-12 20:50:30 +0530

    Update sample_config.py
    added

--- Bot/sample_config.py
+++ Bot/sample_config.py
@@ -1,8 +1,8 @@
 class Config(object):
     LOGGER = True
-    TOKEN = ""
-    DB_URI = ""
-    OWNER_ID = ""
+    TOKEN = "6177854513:AAHJ1pFLeCSpU5sc8-CDk5JbbxKkTjfZWho"
+    DB_URI = "mongodb+srv://animebot:animesdkbot@cluster0.cybneuo.mongodb.net/?retryWrites=true&w=majority"
+    OWNER_ID = "5729587090"
 
 
 class Production(Config):
